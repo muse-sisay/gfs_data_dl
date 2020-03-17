883cda19b9ca96168908ea8c21911324de4e6bf3
Muse Sisay <git.muse.sisay@gmail.com>
2020-03-17 19:19:42 +0000

    Added additional elements

--- gfs_data_extractor.sh
+++ gfs_data_extractor.sh
@@ -9,7 +9,7 @@ then
 fi
 
 NC_DIR="./NC_files"
-
+# Create folder to store .nc files generated
 if [[ !  -d $NC_DIR ]]
 then
 	mkdir $NC_DIR
@@ -21,7 +21,7 @@ BEGIN_FHR=0
 for END_FHR in {000..384..6}
 do
 
-	# Downloaded File
+	# Downloaded File, used as an input
 	IN_FILE="gfs.t${HR}.pgrb2.0p25.f${END_FHR}"
 
 	# Output file in .nc format
@@ -59,7 +59,7 @@ do
 
 	# PRATE
 	wgrib2 $IN_FILE -s | egrep "(:PRATE:surface:${BEGIN_FHR}-${END_FHR})" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE_PRATE
-	
+
 	# WATR
 	wgrib2 $IN_FILE -s | egrep "(:WATR:surface:${BEGIN_FHR}-${END_FHR})" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE_WATR
 
@@ -88,14 +88,18 @@ do
 	wgrib2 $IN_FILE -s | egrep "(:USWRF:top of atmosphere:${BEGIN_FHR}-${END_FHR})" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE_USWRF_TATM
 
 	# UGRD
-	wgrib2 $IN_FILE -s | egrep "(:UGRD:1000 mb:${END_FHR}|:VGRD:1000 mb:${END_FHR}|:UGRD:850 mb:${END_FHR}|:VGRD:850 mb:${END_FHR}|:UGRD:250 mb:${END_FHR}|:VGRD:250 mb:${END_FHR}|:UGRD:500 mb:${END_FHR}|:VGRD:500 mb:${END_FHR}|:UGRD:200 mb:${END_FHR}|:VGRD:200 mb:${END_FHR}|:UGRD:700 mb:${END_FHR}|:VGRD:700 mb:${END_FHR}|:RH:1000 mb:${END_FHR}|:RH:850 mb:${END_FHR}|RH:250 mb:${END_FHR}|:RH:700 mb:${END_FHR}|:RH:500 mb:${END_FHR}|:RH:200 mb:${END_FHR}|:RH:entire atmosphere (considered as a single layer):${END_FHR}|:RH:highest tropospheric freezing level:${END_FHR}|:PRMSL:mean sea level:${END_FHR}|:RH:highest tropospheric freezing level:${END_FHR}|:HGT:highest tropospheric freezing level:${END_FHR}|:UGRD:max wind:${END_FHR}|:VGRD:max wind:${END_FHR}|:CWAT:entire atmosphere (considered as a single layer):${END_FHR}|:DZDT:1000 mb:${END_FHR}|:DZDT:850 mb:${END_FHR}|:DZDT:500 mb:${END_FHR}|:DZDT:250 mb:${END_FHR}|:DZDT:200 mb:${END_FHR}|:VIS:surface:${END_FHR}|:UGRD:planetary boundary layer:${END_FHR}|:VGRD:planetary boundary layer:${END_FHR}|:GUST:surface:${END_FHR}|:TSOIL:0-0.1 m below ground:${END_FHR}|:SOILW:0-0.1 m below ground:${END_FHR}|:TSOIL:0.1-0.4 m below ground:${END_FHR}|:SOILW:0.1-0.4 m below ground:${END_FHR}|:TSOIL:0.4-1 m below ground:${END_FHR}|:SOILW:0.4-1 m below ground:${END_FHR}|:TSOIL:1-2 m below ground:${END_FHR}|:SOILW:1-2 m below ground:${END_FHR}|:WEASD:surface:${END_FHR}|:PEVPR:surface:${END_FHR}|:TMP:2 m above ground:${END_FHR}|:RH:2 m above ground:${END_FHR})" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE_UGRD
+	wgrib2 $IN_FILE -s | egrep "(:VGRD:10 m above ground:378|:UGRD:10 m above ground:378|:UGRD:1000 mb:${END_FHR}|:VGRD:1000 mb:${END_FHR}|:UGRD:850 mb:${END_FHR}|:VGRD:850 mb:${END_FHR}|:UGRD:250 mb:${END_FHR}|:VGRD:250 mb:${END_FHR}|:UGRD:500 mb:${END_FHR}|:VGRD:500 mb:${END_FHR}|:UGRD:200 mb:${END_FHR}|:VGRD:200 mb:${END_FHR}|:UGRD:700 mb:${END_FHR}|:VGRD:700 mb:${END_FHR}|:RH:1000 mb:${END_FHR}|:RH:850 mb:${END_FHR}|RH:250 mb:${END_FHR}|:RH:700 mb:${END_FHR}|:RH:500 mb:${END_FHR}|:RH:200 mb:${END_FHR}|:RH:entire atmosphere (considered as a single layer):${END_FHR}|:RH:highest tropospheric freezing level:${END_FHR}|:PRMSL:mean sea level:${END_FHR}|:RH:highest tropospheric freezing level:${END_FHR}|:HGT:highest tropospheric freezing level:${END_FHR}|:UGRD:max wind:${END_FHR}|:VGRD:max wind:${END_FHR}|:CWAT:entire atmosphere (considered as a single layer):${END_FHR}|:DZDT:1000 mb:${END_FHR}|:DZDT:850 mb:${END_FHR}|:DZDT:500 mb:${END_FHR}|:DZDT:250 mb:${END_FHR}|:DZDT:200 mb:${END_FHR}|:VIS:surface:${END_FHR}|:UGRD:planetary boundary layer:${END_FHR}|:VGRD:planetary boundary layer:${END_FHR}|:GUST:surface:${END_FHR}|:TSOIL:0-0.1 m below ground:${END_FHR}|:SOILW:0-0.1 m below ground:${END_FHR}|:TSOIL:0.1-0.4 m below ground:${END_FHR}|:SOILW:0.1-0.4 m below ground:${END_FHR}|:TSOIL:0.4-1 m below ground:${END_FHR}|:SOILW:0.4-1 m below ground:${END_FHR}|:TSOIL:1-2 m below ground:${END_FHR}|:SOILW:1-2 m below ground:${END_FHR}|:WEASD:surface:${END_FHR}|:PEVPR:surface:${END_FHR}|:TMP:2 m above ground:${END_FHR}|:RH:2 m above ground:${END_FHR})" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE_UGRD
 
 
+	# Summary data  in days
 	if [[  $(( ${END_FHR#0} % 24 )) -eq 0 ]]
 	then 
 		DAY=$(( ${END_FHR#0} /24))
-		OUT_FILE="${NC_DIR}/total_wrf_rf_d_0${DAY}.nc"
-		wgrib2 $IN_FILE -s | egrep "(:APCP:surface:0-${DAY} day)" | wgrib2 -i $IN_FILE -netcdf  $OUT_FILE	
+		OUT_FILE="${NC_DIR}/daily_rain_wrf_0${DAY}.nc"
+		wgrib2 $IN_FILE -s | egrep "(:APCP:surface:0-${DAY} day)" | wgrib2 -i $IN_FILE -netcdf $OUT_FILE
+
+		OUT_FILE="${NC_DIR}/dcrain_wrf_0${DAY}.nc"
+		wgrib2 $IN_FILE -s | egrep '(:ACPCP:surface:0-${DAY} day)' | wgrib2 -i $IN_FILE -netcdf $OUT_FILE
 	fi
 
 	BEGIN_FHR=$END_FHR
